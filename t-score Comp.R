b151708d1a221490074e482e0f2d92a6bd714972
Andrew Herr <54908904+aherr-wsuwinterwheat@users.noreply.github.com>
2019-12-18 10:29:40 -0800

    Update t-score Comp.R

--- t-score Comp.R
+++ t-score Comp.R
@@ -4,20 +4,15 @@ install.packages("naniar")
 install.packages("gglpot2")
 install.packages("reshape2")
 
-library(ggthemes)
 library(reshape2)
 library(naniar)
 library(tidyverse)
 library(ggplot2)
 
-
-Means_Comparison <- read_csv("Means Comparison.csv")
 C92_C18_data <- read_csv("C92-C18_data.csv")
-
 data1 <- replace_with_na_all(data = C92_C18_data, condition = ~.x == ".")
 data1_melt <- melt(data1, id.vars = "VARIETY")
-data1_melt$value = as.numeric(data1_melt$value)
-
+data1_melt$value <- as.numeric(data1_melt$value)
 
 
 ggplot(data = subset(data1_melt,!is.na(value)), aes(x = variable, y = value, fill = VARIETY))+
